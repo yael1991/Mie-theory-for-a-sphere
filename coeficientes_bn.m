4971fff8232cb241e2321781684fef3eabc567da
Yael <yael@MacBook-Pro-de-Yael.local>
2015-04-05 17:18:01 +0200

    calcultarion of Mie cross sections

---
+++ coeficientes_bn.m
@@ -0,0 +1,32 @@
+%%Calcula los bn.
+
+function bn=coeficientes_bn(indice, lambda, n, a)
+
+
+    x=(2*pi*a/lambda);
+    nu=n+0.5;
+    m=indice;
+    mx=m*x;
+    
+        sqmx=sqrt(pi/(2*mx));
+        sqx=sqrt(pi/(2*x));
+        
+        %orden n
+        bmx=besselj(nu,mx)*sqmx;
+        bx=besselj(nu,x)*sqx;
+        by=bessely(nu,x)*sqx;
+        h1x=bx+ 1i*by;
+        
+        %orden n-1 necesarias para derivadas
+        bmx_1=besselj(nu-1,mx)*sqmx;
+        bx_1=besselj(nu-1,x)*sqx;
+        by_1=bessely(nu-1,x)*sqx;
+        h1x_1=bx_1+ 1i* by_1;
+        
+        %derivadas
+        dbmx=mx*bmx_1-n*bmx;
+        dbx=x*bx_1-n*bx;
+        dh1x=x*h1x_1-n*h1x;
+        
+        bn=( bmx*dbx - bx*dbmx ) / ( bmx*dh1x - h1x*dbmx);
+end
---
+++ eficiencia.m
@@ -0,0 +1,238 @@
+
+%Matlab function for calculating Mie absorption, scattergin and
+%extinction cross sections. It also plots the contribuion to these
+%magnitudes of the mie dipolar and quadrupolar coefficients.
+
+function [Q, a1, a2, b1, b2]=eficiencia(indices,lambdas,nmax,a)
+
+
+
+maxi=length(lambdas) %number of wavelength to be considered.
+figure;
+hold on
+for j=1:maxi
+    
+        qsca=0;
+        qext=0;
+       
+        
+        
+        
+        x=(2*pi*a/lambdas (j));
+    
+     
+    for n=1:1:nmax
+        
+        an = coeficientes_an(indices(j),lambdas(j),n,a);
+        plot(lambdas(j),(an*conj(an)),'.r')
+        bn = coeficientes_bn(indices(j),lambdas(j),n,a);
+        plot(lambdas(j),(bn*conj(bn)),'.')
+         
+        
+        if n==1 %dipolar contribution
+            
+            qsca1_a1 = (2*n+1)*(abs(an)^2);
+            qsca1_b1 = (2*n+1)*(abs(bn)^2);
+            qext1_a1 = (2*n+1)*real(an);
+            qext1_b1 = (2*n+1)*real(bn);
+        
+        end
+        
+        
+        if n==2 %quadrupolar contribution
+            
+            qsca1_a2 = (2*n+1)*(abs(an)^2);
+            qsca1_b2 = (2*n+1)*(abs(bn)^2);
+            qext1_a2 = (2*n+1)*real(an);
+            qext1_b2 = (2*n+1)*real(bn);
+        
+        end
+        
+        
+        qsca=qsca+(2*n+1)*(abs(an)^2+abs(bn)^2);
+        qext=qext+(2*n+1)*real(an+bn);
+
+        
+                    
+    end
+   
+    Qsca_a1(j) = (2/x^2)*qsca1_a1;
+    Qsca_b1(j) = (2/x^2)*qsca1_b1;
+    
+    Qsca_a2(j) = (2/x^2)*qsca1_a2;
+    Qsca_b2(j) = (2/x^2)*qsca1_b2;
+    
+    
+    Qext_a1(j) = (2/x^2)*qext1_a1;
+    Qext_b1(j) = (2/x^2)*qext1_b1;
+    
+    Qext_a2(j) = (2/x^2)*qext1_a2;
+    Qext_b2(j) = (2/x^2)*qext1_b2;
+    
+    Qabs_a1(j) = Qext_a1(j) - Qsca_a1(j);
+    Qabs_b1(j) = Qext_b1(j) - Qsca_b1(j);
+    
+    Qabs_a2(j) = Qext_a2(j) - Qsca_a2(j);
+    Qabs_b2(j) = Qext_b2(j) - Qsca_b2(j);
+    
+    
+    
+    Qsca(j)=(2/x^2)*qsca;
+    Qext(j)=(2/x^2)*qext;
+    Qabs(j)=Qext(j)-Qsca(j);
+end
+
+
+Q=[Qext;Qsca;Qabs] ;
+ev= 1242./lambdas;
+
+
+%limits for de plots
+xmin = min(lambdas);
+xmax = max(lambdas);
+xmin_ev = min(ev);
+xmax_ev = max(ev);
+ymax_s = max(Qsca)+0.2;
+ymax_e = max(Qabs)+0.2;
+ymax_a = max(Qext)+0.2;
+
+
+
+figure;
+plot(lambdas,Qsca,'r')
+hold on
+plot(lambdas,Qext,'b')
+plot(lambdas,Qabs,'g')
+hold off
+xlabel('energy (eV)','FontSize',20);
+ylabel('Q','FontSize',20);
+l=legend('Q_s_c_a', 'Q_e_x_t', 'Q_a_b_s','Location','northeast' );
+set(l,'FontSize',20);
+set(gca,'FontSize',16);
+axis([xmin xmax 0 max([ymax_s, ymax_a, ymax_e])]);
+legend boxoff;
+title('Cross sections')
+box on;
+hold off;
+
+
+
+%%%%%%%%%% scattering %%%%%%%%%%%%
+
+figure;
+plot(ev, Qsca_a1, 'LineWidth',2)
+hold on
+plot(ev, Qsca_b1, 'r', 'LineWidth',2)
+plot(ev, Qsca_a2, 'g', 'LineWidth',2)
+plot(ev, Qsca_b2, 'c', 'LineWidth',2)
+plot(ev,Qsca,'m', 'LineWidth',2)
+xlabel('energy (eV)','FontSize',20);
+ylabel('Q_s_c_a','FontSize',20);
+l=legend('a1','b1', 'a2', 'b2', 'Q_s_c_a','Location','northeast' );
+set(l,'FontSize',20);
+set(gca,'FontSize',16);
+axis([xmin xmax 0 ymax_s])
+legend boxoff;
+title('Scattering cross section')
+box on;
+hold off;
+
+figure;
+plot(lambdas, Qsca_a1, 'LineWidth',2)
+hold on
+plot(lambdas, Qsca_b1, 'r', 'LineWidth',2)
+plot(lambdas, Qsca_a2, 'g', 'LineWidth',2)
+plot(lambdas, Qsca_b2, 'c', 'LineWidth',2)
+plot(lambdas,Qsca,'m', 'LineWidth',2)
+xlabel('wavelength (nm)','FontSize',20);
+ylabel('Q_s_c_a','FontSize',20);
+l=legend('a1','b1', 'a2', 'b2', 'Q_s_c_a','Location','northwest' );
+set(l,'FontSize',20);
+set(gca,'FontSize',16);
+axis([xmin_ev xmax_ev 0 ymax_s])
+legend boxoff;
+title('Scattering cross section')
+box on;
+hold off;
+
+
+
+%%%%%%%%%%%% extinction %%%%%%%%%%%%
+
+
+
+figure;
+plot(ev, Qext_a1, 'LineWidth',2)
+hold on
+plot(ev, Qext_b1, 'r', 'LineWidth',2)
+plot(ev, Qext_a2, 'g', 'LineWidth',2)
+plot(ev, Qext_b2, 'c', 'LineWidth',2)
+plot(ev,Qext,'m', 'LineWidth',2)
+xlabel('energy (eV)','FontSize',20);
+ylabel('Q_e_x_t','FontSize',20);
+l=legend('a1','b1', 'a2', 'b2', 'Q_e_x_t','Location','northwest' );
+set(l,'FontSize',20);
+set(gca,'FontSize',16);
+axis([xmin xmax 0 ymax_e])
+legend boxoff;
+title('Extinction cross section')
+box on;
+hold off;
+
+figure;
+plot(lambdas, Qext_a1, 'LineWidth',2)
+hold on
+plot(lambdas, Qext_b1, 'r', 'LineWidth',2)
+plot(lambdas, Qext_a2, 'g', 'LineWidth',2)
+plot(lambdas, Qext_b2, 'c', 'LineWidth',2)
+plot(lambdas,Qext,'m', 'LineWidth',2)
+xlabel('wavelength (nm)','FontSize',20);
+ylabel('Q_e_x_t','FontSize',20);
+l=legend('a1','b1', 'a2', 'b2', 'Q_e_x_t','Location','northeast' );
+set(l,'FontSize',20);
+set(gca,'FontSize',16);
+axis([xmin_ev xmax_ev 0 ymax_e])
+legend boxoff;
+title('Extinction cross section')
+box on;
+hold off;
+
+
+%%%%%%%%% absorption %%%%%%%%%
+
+
+figure;
+plot(ev, Qabs_a1, 'LineWidth',2)
+hold on
+plot(ev, Qabs_b1, 'r', 'LineWidth',2)
+plot(ev, Qabs_a2, 'g', 'LineWidth',2)
+plot(ev, Qabs_b2, 'c', 'LineWidth',2)
+plot(ev,Qabs,'m', 'LineWidth',2)
+xlabel('energy (eV)','FontSize',20);
+ylabel('Q_a_b_s','FontSize',20);
+l=legend('a1','b1', 'a2', 'b2', 'Q_a_b_s','Location','northwest' );
+set(l,'FontSize',20);
+set(gca,'FontSize',16);
+axis([xmin xmax 0 ymax_a])
+legend boxoff;
+title('Absorption cross section')
+box on;
+hold off;
+
+figure;
+plot(lambdas, Qabs_a1, 'LineWidth',2)
+hold on
+plot(lambdas, Qabs_b1, 'r', 'LineWidth',2)
+plot(lambdas, Qabs_a2, 'g', 'LineWidth',2)
+plot(lambdas, Qabs_b2, 'c', 'LineWidth',2)
+plot(lambdas, Qabs,'m', 'LineWidth',2)
+xlabel('wavelength (nm)','FontSize',20);
+ylabel('Q_a_b_s','FontSize',20);
+l=legend('a1','b1', 'a2', 'b2', 'Q_a_b_s','Location','northeast' );
+set(l,'FontSize',20);
+set(gca,'FontSize',16);
+axis([xmin_ev xmax_ev 0 ymax_a])
+legend boxoff;
+title('Absorption cross section')
+box on;
+hold off;
